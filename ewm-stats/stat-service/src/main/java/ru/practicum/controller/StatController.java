bfcd1e66e55d3abc22bfa0d9c646e6b24df59cb7
SavaStyle <sava.90@inbox.ru>
2023-05-13 11:20:52 +0300

    stat 0.1

---
+++ ewm-stats/stat-service/src/main/java/ru/practicum/controller/StatController.java
@@ -0,0 +1,33 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.EndpointHitDto;
+import ru.practicum.model.ViewStats;
+import ru.practicum.service.StatService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatController {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final StatService statService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public EndpointHitDto create(@RequestBody EndpointHitDto endpointHitDto) {
+        return statService.saveStat(endpointHitDto);
+    }
+
+    @GetMapping("/stats")
+    public List<ViewStats> getStats(@RequestParam(required = false) String start,
+                                    @RequestParam(required = false) String end,
+                                    @RequestParam(required = false) List<String> uris,
+                                    @RequestParam(defaultValue = "false") Boolean unique) {
+        return statService.getStats(LocalDateTime.parse(start, FORMATTER), LocalDateTime.parse(end, FORMATTER), uris, unique);
+    }
+}
